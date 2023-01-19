437fdc605b4e2d428a278977baa6bb6ea3e7d072
seog-jun <shong69@myseneca.ca>
2023-01-19 01:09:42 -0500

    Added loading username

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDEEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDEEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+function paintGreetings(username) {
   greeting.innerText = `Hello! ${username}`;
   greeting.classList.remove(HIDEEN_CLASSNAME);
 }
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDEEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
