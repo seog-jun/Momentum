59386de337f8c4d6d20d382d28162ec87c2c9bbb
seog-jun <shong69@myseneca.ca>
2023-01-19 00:16:35 -0500

    Added greeting to the user with username

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDEEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDEEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello! ${username}`;
+  greeting.classList.remove(HIDEEN_CLASSNAME);
 }
 loginForm.addEventListener("submit", onLoginSubmit);
