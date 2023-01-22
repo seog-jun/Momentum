1a86f27e055217e9b076b37ea1dc194492cae4ce
seog-jun <shong69@myseneca.ca>
2023-01-21 20:06:54 -0500

    Delete todo

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.querySelector("#todo-form input");
+const todoList = document.getElementById("todo-list");
+
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.append(btn);
+  todoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintToDo(newTodo);
+}
+
+todoForm.addEventListener("submit", handleToDoSubmit);
