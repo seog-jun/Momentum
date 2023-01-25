f464c9e8556d9ef3988e801860e0cf98a62d6d96
seog-jun <shong69@myseneca.ca>
2023-01-25 13:09:19 -0500

    modified background

--- js/background.js
+++ js/background.js
@@ -1,4 +1,16 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "0.jpeg",
+  "1.jpeg",
+  "3.jpeg",
+  "4.jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+  "10.jpeg",
+  "11.jpeg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
