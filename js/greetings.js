dad4702f0555d95182d88bd2172303519cce1464
seog-jun <shong69@myseneca.ca>
2023-01-25 13:39:13 -0500

    Modified greeting part

--- js/greetings.js
+++ js/greetings.js
@@ -1,27 +1,39 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
 const greeting = document.querySelector("#greeting");
+const btn = document.querySelector("#changeBtn");
 
-const HIDEEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDEEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
 function paintGreetings(username) {
   greeting.innerText = `Hello! ${username}`;
-  greeting.classList.remove(HIDEEN_CLASSNAME);
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  btn.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDEEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
+
+function changeUsername() {
+  greeting.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  btn.classList.add(HIDDEN_CLASSNAME);
+  localStorage.removeItem(USERNAME_KEY);
+  loginInput.value = "";
+}
+
+btn.addEventListener("click", changeUsername);
