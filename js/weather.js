d12c4c6ac172e96e0a1fada9292a66fcf7c115de
seog-jun <shong69@myseneca.ca>
2023-01-25 13:00:56 -0500

    Added celsius

--- js/weather.js
+++ js/weather.js
@@ -8,8 +8,10 @@ function onGeoOk(position) {
     response.json().then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${Math.round(
+        data.main.temp
+      )}℃`;
+      city.innerText = data.name + " /";
     })
   );
 }
