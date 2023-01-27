813c5d77136f6197f45c042809c2f13282c7c5b0
seog-jun <shong69@myseneca.ca>
2023-01-27 01:22:45 -0500

    Added check box

--- js/todo.js
+++ js/todo.js
@@ -22,10 +22,17 @@ function paintToDo(todoObj) {
   const li = document.createElement("li");
   li.id = todoObj.id;
   const span = document.createElement("span");
-  span.innerText = todoObj.text;
+  span.innerText = `  ${todoObj.text}`;
+  const check = document.createElement("span");
+  if (todoObj.check) {
+    check.innerText = "✔" + " ";
+  } else {
+    check.innerText = "☐" + " ";
+  }
   const btn = document.createElement("button");
   btn.innerText = "❌";
   btn.addEventListener("click", deleteTodo);
+  li.appendChild(check);
   li.appendChild(span);
   li.append(btn);
   todoList.appendChild(li);
@@ -38,6 +45,7 @@ function handleToDoSubmit(event) {
   const newTodoObj = {
     id: Date.now(),
     text: newTodo,
+    check: false,
   };
   todos.push(newTodoObj);
   paintToDo(newTodoObj);
@@ -52,4 +60,34 @@ if (savedToDos) {
   const parsedToDos = JSON.parse(savedToDos);
   todos = parsedToDos;
   parsedToDos.forEach(paintToDo);
+}
+
+const todoCheck = todoList.querySelectorAll("span");
+
+function handleCheckBox(event) {
+  const li = event.target.parentElement;
+  console.log(li.id); //string
+  console.log(typeof li.id);
+  console.log(typeof todos[0].id);
+  for (let i = 0; todos.length; i++) {
+    if (todos[i].id === parseInt(li.id)) {
+      if (todos[i].check === true) {
+        console.log("uncheck");
+        todos[i].check = false;
+        console.log(todos[i]);
+        saveToDos();
+        location.reload();
+      } else {
+        console.log("check");
+        todos[i].check = true;
+        console.log(todos[i]);
+        saveToDos();
+        location.reload();
+      }
+    }
+  }
+}
+
+for (let i = 0; i < todoCheck.length; i += 2) {
+  todoCheck[i].addEventListener("click", handleCheckBox);
 }
